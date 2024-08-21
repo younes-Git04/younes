7a4698cda587afb6bf14bb76613400066b4179a3
younes-Git04 <ammmerzouguiyounes@gmail.com>
2024-08-21 08:36:20 -0700

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,25 +1,21 @@
-  import React, { useEffect, useState } from "react";
-  import { StyleSheet,View,Button,Modal } from "react-native";
-  import { TextInput } from "react-native-gesture-handler";
-  import AntDesign from '@expo/vector-icons/AntDesign';
-  const Index=()=>{ 
-    const [show, setShow] = useState(false);
-
-    return(
-      <View style={{flex:1,alignItems:'center',justifyContent:'center',backgroundColor:'#7C93C3'}}>
-        <Button title="اضغط هنا" onPress={()=>setShow(true)}/>
-        <Modal visible={show} style={{position:'relative'}}>
-        <View style={{backgroundColor:'#1E2A5E',padding:50}}>
-          <h1 style={{color:'#fff',fontWeight:'bold',fontSize:40,textAlign:"center"}}>Log In</h1>
-          <TextInput style={{marginBottom:11,width:'90%',alignSelf:'center',textAlignVertical:'top',height:40}}
-          placeholder="Enter Your Email" placeholderTextColor="grey"/>
-          <TextInput style={{marginBottom:11,width:'90%',alignSelf:'center',textAlignVertical:'top',height:40}}
-          placeholder="Enter Your Password" placeholderTextColor="grey"/>
-          <Button title="send"/>
-          <AntDesign name="closecircleo" size={24} color="white" onPress={()=>setShow(false)} style={{position:'absolute',zIndex:999999,right:20}} />
-        </View>
-        </Modal>
+import React from "react";
+import { StyleSheet,View,Text,Image } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Avatar, Drawer, IconButton, List } from "react-native-paper";
+const Index=()=>{
+  return(
+    <View style={{backgroundColor:'lightblue',flexDirection:'row'
+    }}>
+      <View style={{flex:1,padding:40,backgroundColor:'#821131',alignSelf:'center'}}>
+        <Avatar.Icon icon="login" style={{backgroundColor:'#fff',alignSelf:'center'}} color="red" size={90}/>
+        <br /><br /><br /><br />
+          <TextInput style={{backgroundColor:'#ccc',height:35}} placeholderTextColor="grey" placeholder="Enter Username"/>
+          <br /><br />
+          <TextInput style={{backgroundColor:'#ccc',height:35}} placeholder="Enter Password" secureTextEntry placeholderTextColor="grey"/>
+          <br /><br />
+          <Drawer.Item icon="send"  label="SEND" style={{backgroundColor:'#64ffda',}}/>
       </View>
-    );
-  }
-  export default Index;
+    </View>
+  );
+}
+export default Index;
